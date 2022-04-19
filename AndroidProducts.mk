fabb0a128b2276218a54500cb31235beee832dbb
NotZeetaa <rodrigo2005contente@gmail.com>
2022-04-19 13:32:44 +0000

    lmi: Havoc bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lmi.mk
+    $(LOCAL_DIR)/havoc_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lmi-user \
-    lineage_lmi-userdebug \
-    lineage_lmi-eng
+    havoc_lmi-user \
+    havoc_lmi-userdebug \
+    havoc_lmi-eng
